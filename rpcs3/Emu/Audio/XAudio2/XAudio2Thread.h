ae70e032e9fcb32891908786716d9c79a807f12c
Zangetsu38 <Dante38@free.fr>
2015-11-16 13:21:56 +0100

    Fix Xaudio2 for Windows 8 and Windows 10.

--- rpcs3/Emu/Audio/XAudio2/XAudio2Thread.h
+++ rpcs3/Emu/Audio/XAudio2/XAudio2Thread.h
@@ -2,9 +2,20 @@
 
 #ifdef _WIN32
 
+#include "3rdparty/minidx12/Include/sdkddkver.h" // Use last SDKDDKVer in SDK 10.586 for Win10
 #include "Emu/Audio/AudioThread.h"
 
-#include "3rdparty/XAudio2_7/XAudio2.h"
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN8) // XAudio2 2.8 and 2.9 available only on Win8+ and Win10.
+#include "3rdparty/minidx12/Include/xaudio2.h"
+#else 
+#include "3rdparty/XAudio2_7/XAudio2.h" // XAudio 2.7 for Windows 7 Only
+#endif
+
+#if (_WIN32_WINNT >= _WIN32_WINNT_WIN10)
+#pragma comment(lib,"../3rdparty/minidx12/Lib/xaudio2.lib") // Lib 2.9 for Only Windows 10
+#elif (_WIN32_WINNT >= _WIN32_WINNT_WIN8)
+#pragma comment(lib,"../3rdparty/minidx12/Lib/xaudio2_8.lib") // Lib 2.8 for Only Windows 8+
+#endif
 
 class XAudio2Thread : public AudioThread
 {
